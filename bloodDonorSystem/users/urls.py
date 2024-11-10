defaecb2674c2f89adaaf4139579f7f77da2747f
Jeff-Maina <devyoungblud@gmail.com>
2024-11-10 12:11:09 +0300

    fix: remove broken link

--- bloodDonorSystem/users/urls.py
+++ bloodDonorSystem/users/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home_view, name="user-dashboard"),
     path("register/", views.register_user, name="register"),
     path("complete-profile/", views.complete_profile, name="complete-profile"),
     path("dashboard/", views.dashboard_view, name='user-dashboard'),
